8e3cffea8d0aa5db69b69d8defe85322bf00c5bd
aimperatori <aimperatori@ucs.br>
2023-10-10 22:06:54 -0300

    add post feature to NewsCheckerfetcher

--- webpage/src/api/NewsChecker/NewsCheckerFetcher.js
+++ webpage/src/api/NewsChecker/NewsCheckerFetcher.js
@@ -5,4 +5,26 @@ export class NewsCheckerFetcher {
         return fetch('https://localhost:7113/' + endpoint);
     }
 
+    static async Post(endpoint, formData) {
+        return fetch('https://localhost:7113/' + endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+            /*
+            .then((response) => response.json())
+            .then((data) => {
+                // Manipule a resposta da API conforme necessário
+                console.log(data);
+                this.setState({ loading: false });
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar o formulário:', error);
+                this.setState({ loading: false });
+            });
+            */
+    }
+
 }
