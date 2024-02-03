84cde9a48101cd64f110e727c660c228eec64e9a
aimperatori <aimperatori@ucs.br>
2024-02-03 19:10:04 -0300

    added basic search on the news

---
+++ NewsChecker/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsChecker.Services;
+using System.Collections;
+
+namespace NewsChecker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly SearchService _searchSerive;
+
+        public SearchController(SearchService searchSerive)
+        {
+            _searchSerive = searchSerive;
+        }
+
+        [HttpGet("basic")]
+        public IActionResult SearchBasic([FromQuery] string text)
+        {
+            IEnumerable list = _searchSerive.BasicSearch(text);
+
+            return Ok(list);
+        }
+    }
+}
---
+++ NewsChecker/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsChecker.Data;
+
+namespace NewsChecker.Services
+{
+    public class SearchService
+    {
+        private readonly NewsCheckerContext _context;
+
+        public SearchService(NewsCheckerContext context)
+        {
+            _context = context;
+        }
+
+        internal IQueryable BasicSearch(string text)
+        {
+            var searchWords = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            IQueryable query = _context.News.Select(_ => _);
+
+            foreach (var word in searchWords)
+            {
+                query = _context.News.Where(_ => EF.Functions.Like(_.Title, $"%{word}%") ||
+                                                 EF.Functions.Like(_.Subtitle, $"%{word}%") ||
+                                                 EF.Functions.Like(_.Resume, $"%{word}%"));
+            }
+
+            return query;
+        }
+    }
+}
